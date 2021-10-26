e78d595f1c7826f45cb301bd432d7ecf86f7836f
Angurajdin <angurajdineshb@gmail.com>
2021-10-26 21:52:06 +0530

    login,signup frontEnd merged

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { useEffect,useState } from 'react';
+import { useState } from 'react';
 import '../css/loginScreen.css';
 import Axios from 'axios';
 import userObj from '../config/user_credentials';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import logo from '../assets/img/Finalwhitelogo.png'
+
+
 function LoginScreen() {
-    const [emailState,setemailState]=useState('');
-    const [passwordState,setpasswordState]=useState('');
+    const [emailid,setEmailid]=useState('');
+    const [password,setPassword]=useState('');
     const history = useHistory();
     const submitFunction=()=>{
         Axios.post('/api/login',{
-            email:emailState,
-            password:passwordState
+            email:emailid,
+            password:password
         }).then((result)=>{
             if(result.data.success){
                 userObj.username = result.data.username;
@@ -23,14 +26,77 @@ function LoginScreen() {
             console.log(err);
         });
     }
+        // return (
+        //     <div className="login-area">
+        //         <input type="text" name="email" placeholder="Enter Your Email" onChange={(e)=>{setemailState(e.target.value)}}/>
+        //         <input type="password" name="password" placeholder="Enter Your Password" onChange={(e)=>{setpasswordState(e.target.value)}}/>
+        //         <button type="submit" onClick={submitFunction}> Submit </button>
+        //         <a href="/"> Back to Homepage </a>
+        //     </div>
+        // );
+    
+    
         return (
+        <div className="container loginTop">
+            <div className="row">
+                <div className="col-md-12 col-lg-9">
+                    <div className="col"></div>
+                    <div className="col-sm-10 col-md-8 leftbox">
+                        <div className="row">
+            <div className="col"></div>
+            <div className="col-6 login">
+            <center className="mb-4 loginTitle">Log In</center>
+                <div className="mb-3">
+                <label className="form-label formLabel">Email address</label>
+                    <input type="email" value={emailid} onChange={e => setEmailid(e.target.value)} className="form-control inputField" id="Email1"/>
+                </div>
+                <div className="mb-3">
+                <label className="form-label formLabel">Password</label>
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control inputField" id="Password1"/>
+                </div>
 
-            <div className="login-area">
-                <input type="text" name="email" placeholder="Enter Your Email" onChange={(e)=>{setemailState(e.target.value)}}/>
-                <input type="password" name="password" placeholder="Enter Your Password" onChange={(e)=>{setpasswordState(e.target.value)}}/>
-                <button type="submit" onClick={submitFunction}> Submit </button>
-                <a href="/"> Back to Homepage </a>
+                <div className="row">
+                    <div className="col">
+                        Not a member? <Link to="/signup">Register</Link> 
+                    </div>
+                    <div className="col">
+                        <Link style={{float:"right"}} to="/forgetpassword">Forget Password?</Link>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center">
+                <div className="col"></div>
+                    <div className="col">
+                    <button type="submit" onClick={submitFunction} className="mt-4 btn btn-primary loginbutton">Login</button>
+                    </div>
+                <div className="col"></div>
+                </div>
             </div>
-        );
+            <div className="col"></div>
+        </div>
+                    </div>
+                    <div className="col"></div>
+                </div>
+                
+                    <div className="col-lg-3 rightbox textStyle d-none d-lg-block d-xl-block">
+                        <br/><br/><br/>
+                        <br/><br/><br/>
+                    <div className="img_container">
+                        <center>
+                            <img src={logo} alt="can't fetch"/>
+                        </center>
+                            <br/><br/><br/>
+                            <div className="text-block">
+                                <h4><b>Psg Tech Coding Club</b></h4>
+                                Simple things should be simple, complex
+                                things should be possible.
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+  
+    );
+    
 }
 export default LoginScreen
--- client/src/components/SignUpScreen.js
+++ client/src/components/SignUpScreen.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import '../css/signupScreen.css';
-import {useState,useEffect} from 'react';
-import Axios  from 'axios';
+import {useState} from 'react';
+import Axios from 'axios';
+import '../css/signupScreen.css'
+import { Link } from 'react-router-dom';
+import logo from '../assets/img/Finalwhitelogo.png'
+
+
 function SignUpScreen() {
-    const [email,setemail]=useState('');
-    const [password,setpassword]=useState('');
-    const [username,setusername]=useState('');
-    const [name,setname]=useState('');
+    const [emailid,setEmailid]=useState('');
+    const [password,setPassword]=useState('');
+    const [username,setUsername]=useState('');
+    // const [name,setname]=useState('');
 
     const submitfunction = ()=>{
         Axios.post('/api/login/add',{
-            email:email,
+            email:emailid,
             password:password,
             username:username,
-            name:name
+            // name:name
         }).then((result)=>{
             if(result.data.success){
                 alert('Successfully Added!!');
@@ -25,15 +30,75 @@ function SignUpScreen() {
             console.log(err.toString());
         });
     }
+    // return (
+    //     <div className="signup-screen">
+    //         <input type="text" placeholder="Enter Name" name="name" onChange={(e)=>{setname(e.target.value)}}/>
+    //         <input type="text" placeholder="Enter NickName" name="username" onChange={(e)=>{setusername(e.target.value)}}/>
+    //         <input type="text" placeholder="Enter Email" name="email" onChange={(e)=>{setemail(e.target.value)}}/>
+    //         <input type="password" placeholder="Enter Password" name="password" onChange={(e)=>{setpassword(e.target.value)}}/>
+    //         <button type="submit" onClick={submitfunction}> Submit </button>
+    //     </div>
+    // )
     return (
-        <div className="signup-screen">
-            <input type="text" placeholder="Enter Name" name="name" onChange={(e)=>{setname(e.target.value)}}/>
-            <input type="text" placeholder="Enter NickName" name="username" onChange={(e)=>{setusername(e.target.value)}}/>
-            <input type="text" placeholder="Enter Email" name="email" onChange={(e)=>{setemail(e.target.value)}}/>
-            <input type="password" placeholder="Enter Password" name="password" onChange={(e)=>{setpassword(e.target.value)}}/>
-            <button type="submit" onClick={submitfunction}> Submit </button>
+        <div className="container loginTop">
+            <div className="row">
+                <div className="col-md-12 col-lg-9">
+                    <div className="col"></div>
+                    <div className="col-sm-10 col-md-8 leftbox">
+                        <div className="row">
+            <div className="col"></div>
+            <div className="col-6 login">
+            <center className="mb-4 loginTitle">Sign Up</center>
+                <div className="mb-3">
+                <label className="form-label formLabel">Username</label>
+                    <input type="text" value={username} onChange={e => setUsername(e.target.value)} className="form-control inputField" id="UserName1"/>
+                </div>
+                <div className="mb-3">
+                <label className="form-label formLabel">Email address</label>
+                    <input type="email" value={emailid} onChange={e => setEmailid(e.target.value)} className="form-control inputField" id="Email1"/>
+                </div>
+                <div className="mb-3">
+                <label className="form-label formLabel">Password</label>
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control inputField" id="Password1"/>
+                </div>
+                <center>
+                <div className="row">
+                    <div className="col" >
+                        <button type="submit" onClick={submitfunction} className="mt-4 btn btn-primary loginbutton">Signup</button>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col" >
+                        Already Registered ? <Link to="/login">Login</Link>
+                    </div>                    
+                </div>
+                </center>
+            </div>
+            <div className="col"></div>
         </div>
-    )
+                    </div>
+                    <div className="col"></div>
+                </div>
+                
+                    <div className="col-lg-3 rightbox textStyle d-none d-lg-block d-xl-block">
+                        <br/><br/><br/>
+                        <br/><br/><br/>
+                    <div className="img_container">
+                        <center>
+                            <img src={logo} alt="can't fetch"/>
+                        </center>
+                            <br/><br/><br/>
+                            <div className="text-block">
+                                <h4><b>Psg Tech Coding Club</b></h4>
+                                Simple things should be simple, complex
+                                things should be possible.
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+  
+    );
 }
 
 export default SignUpScreen
