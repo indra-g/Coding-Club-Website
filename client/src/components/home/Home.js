6c768cff94c67a72835e84210fd644834dbdf52d
indra-g <indrashekar405@gmail.com>
2021-12-15 11:50:31 +0530

    Completed Home Screen Frontend

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -10,29 +10,32 @@ const Home = () => {
         if (page === "events-home") history.push("/events-home");
     }
     return (
-        <div>
-            <nav id="home-navbar">
-                <h1 className="logo">
-                    <img src={PSGLogo} alt="PSG logo" width={"50px"} height={"50px"}/>
-                    {/*<span className="text-primary">P</span>SG COLLEGE OF TECHNOLOGY*/}
-                    PSG COLLEGE OF TECHNOLOGY
-                </h1>
-            </nav>
+      <div>
+        <nav id="home-navbar">
+          <h1 className="logo heading">
+            <img src={PSGLogo} alt="PSG logo" width={"50px"} height={"50px"} /> 
+            {/*<span className="text-primary">P</span>SG COLLEGE OF TECHNOLOGY*/}
+            PSG COLLEGE OF TECHNOLOGY
+          </h1>
+        </nav>
 
-            <header id="home-showcase">
-                <div className="showcase-content">
-                    <img src={CodingClubLogo} alt="Coding club Logo"/>
-                        <h2 className="l-leading">
-                            <span className="text-primary">CODING </span> CLUB PSGCT
-                        </h2>
-                        <p className="lead">
-                            “Path to the land of Binaries.”
-                        </p>
-                        {/*<Link to={"/events-home"} className="btn">Explore</Link>*/}
-                        <button onClick={() => goToPage("events-home")} className="home-btn">Explore</button>
-                </div>
-            </header>
-        </div>
+        <header id="home-showcase">
+          <div className="showcase-content">
+            <img className="img" src={CodingClubLogo} alt="Coding club Logo" />
+            <h2 className="l-leading">
+              <span className="text">CODING </span> CLUB PSGCT
+            </h2>
+            <p className="lead">“Path To The Land Of Binaries”</p>
+            {/*<Link to={"/events-home"} className="btn">Explore</Link>*/}
+            <button
+              onClick={() => goToPage("events-home")}
+              className="btn btn-primary"
+            >
+              Explore
+            </button>
+          </div>
+        </header>
+      </div>
     );
 };
 
